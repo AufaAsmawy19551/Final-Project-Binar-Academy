2c5b3c3f3ae613b8a2368f1f76b9c60205976c89
AufaAsmawy19551 <aufamuhammad19551@gmail.com>
2023-06-06 19:40:20 +0700

    update transaction seeder

--- src/database/seeders/20230606112520-Transaction.js
+++ src/database/seeders/20230606112520-Transaction.js
@@ -2,11 +2,37 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    // await queryInterface.bulkInsert('Transactions', [], {});
-  },
+	async up(queryInterface, Sequelize) {
+		await queryInterface.bulkInsert(
+			'Transactions',
+			[
+				{
+					customer_id: 1,
+					date: new Date(),
+					status: 'paid',
+					createdAt: new Date(),
+					updatedAt: new Date(),
+				},
+				{
+					customer_id: 1,
+					date: new Date(),
+					status: 'unpaid',
+					createdAt: new Date(),
+					updatedAt: new Date(),
+				},
+				{
+					customer_id: 1,
+					date: new Date(),
+					status: 'cancelled',
+					createdAt: new Date(),
+					updatedAt: new Date(),
+				},
+			],
+			{}
+		);
+	},
 
-  async down (queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('Transactions', null, {});
-  }
+	async down(queryInterface, Sequelize) {
+		await queryInterface.bulkDelete('Transactions', null, {});
+	},
 };

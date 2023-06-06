c653f0385e6e6a6d9da5823b9d926652da60c7f3
Rizky Maolana  Firdaus <rizkymaulanafrds@gmail.com>
2023-06-06 21:10:35 +0700

    update seeder flight, route

--- src/database/seeders/20230606102241-Route.js
+++ src/database/seeders/20230606102241-Route.js
@@ -7,11 +7,132 @@ module.exports = {
       "Routes",
       [
         {
+          departure_airport_id: 1,
+          arrival_airport_id: 2,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
           departure_airport_id: 2,
           arrival_airport_id: 3,
           createdAt: new Date(),
           updatedAt: new Date(),
         },
+        {
+          departure_airport_id: 3,
+          arrival_airport_id: 4,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          departure_airport_id: 5,
+          arrival_airport_id: 6,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          departure_airport_id: 6,
+          arrival_airport_id: 7,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          departure_airport_id: 7,
+          arrival_airport_id: 8,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          departure_airport_id: 8,
+          arrival_airport_id: 9,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          departure_airport_id: 9,
+          arrival_airport_id: 10,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          departure_airport_id: 10,
+          arrival_airport_id: 11,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          departure_airport_id: 11,
+          arrival_airport_id: 12,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          departure_airport_id: 10,
+          arrival_airport_id: 9,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          departure_airport_id: 5,
+          arrival_airport_id: 8,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          departure_airport_id: 6,
+          arrival_airport_id: 5,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          departure_airport_id: 4,
+          arrival_airport_id: 7,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          departure_airport_id: 7,
+          arrival_airport_id: 4,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          departure_airport_id: 3,
+          arrival_airport_id: 6,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          departure_airport_id: 3,
+          arrival_airport_id: 9,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          departure_airport_id: 4,
+          arrival_airport_id: 5,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          departure_airport_id: 3,
+          arrival_airport_id: 7,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          departure_airport_id: 6,
+          arrival_airport_id: 10,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          departure_airport_id: 8,
+          arrival_airport_id: 7,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+
       ],
       {}
     );

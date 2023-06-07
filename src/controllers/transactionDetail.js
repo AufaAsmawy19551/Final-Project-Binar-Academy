4ae30bf6bcd013e05377dea3c8ec62c9e7bade78
yasirr <m.yasirriziq@gmail.com>
2023-06-07 22:46:36 +0700

    updated

--- src/controllers/transactionDetail.js
+++ src/controllers/transactionDetail.js
@@ -1,5 +1,5 @@
 const modelName = 'TransactionDetail';
-const { TransactionDetail: Model, Airplane, sequelize } = require('../database/models');
+const { TransactionDetail: Model, sequelize } = require('../database/models');
 const Validator = require('../utils/validatorjs');
 
 module.exports = {

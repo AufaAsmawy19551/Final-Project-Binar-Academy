7e4e15421081dc55dd2950625b2c41e2dc220672
Rizky Maolana  Firdaus <rizkymaulanafrds@gmail.com>
2023-06-09 20:39:30 +0700

    feat: update register

--- src/controllers/authCustomer.js
+++ src/controllers/authCustomer.js
@@ -1,6 +1,6 @@
 const modelName = 'Customer';
 const bycrypt = require("bcrypt")
-const { Customer: Model,Customer, sequelize } = require('../database/models');
+const { Customer: Model, Customer, sequelize } = require('../database/models');
 const Validator = require('../utils/validatorjs');
 
 module.exports = {
@@ -9,8 +9,8 @@ module.exports = {
 			const validation = await Validator.validate(req.query, {
 				name: 'required|string|exist:Cutomers,id',
 				email: 'required|unique|string|exist:Customers,id',
-				password: 'required|string|min:8|max:255|confirmed',
-				password_confirmation: 'required|string|min:8|max:255|confirmed',
+				password: 'required|string|between8,255|confirmed',
+				// "password_confirmation": 'required|string|min:8|max:255|confirmed',
 			});
 
 			if (validation.failed) {
@@ -23,15 +23,19 @@ module.exports = {
 			const hashPassword = await bycrypt(password,10)
 			// buat service dan response di sini!
 
-			return res.status(201).json({
-				status: true,
-				message: "User Created!",
+			const customer = await Customer.create(req.body,{
+				name, email, password: hashPassword
+			});
+
+			return res.status(200).json({
+				success: true,
+				message: `Success create new ${modelName}!`,
 				data: {
-					id: validation.id,
-					name: validation.name,
-					email: validation.email
-				}
-			})
+					name: customer.name,
+					email: customer.email,
+				},
+			  });
+
 		} catch (error) {
 			next(error);
 		}

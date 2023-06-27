44914bdbf4d02f4a5db3457b1f54290236b01d80
AufaAsmawy19551 <aufamuhammad19551@gmail.com>
2023-06-27 23:08:07 +0700

    update

--- src/controllers/flight.js
+++ src/controllers/flight.js
@@ -19,6 +19,7 @@ module.exports = {
         departure_date: 'required|date',
         number_passenger: 'required|integer|min:1',
         class_id: 'required|integer|exist:Classes,id',
+        is_promo: 'required|boolean',
       })
 
       if (validation.failed) {
@@ -29,58 +30,137 @@ module.exports = {
         })
       }
 
-      const list = await sequelize.query(
-        `
-			  SELECT 
-          f.id,
-			  	dpA.name "departure_airport",
-			  	dpA.code "departure_airport_code",
-			  	dpC.name "departure_city",
-			  	dpC.code "departure_city_code",
-			  	dpC.time_zone "departure_city_time_zone",
-			  	f.departure_date,
-			  	arA.name "arrival_airport",
-			  	arA.code "arrival_airport_code",
-			  	arC.name "arrival_city",
-			  	arC.code "arrival_city_code",
-			  	arC.time_zone "arrival_city_time_zone",
-			  	f.arrival_date,
-			  	a.name "airplane_name",
-			  	a.logo "airplane_logo",
-			  	a.code "airplane_code",
-			  	c.name "airplane_class",
-			  	f.price,
-			  	f.discount,
-			  	f.tax,
-			  	f.stock - (
-            SELECT 
-              COUNT(id)
-            FROM
-              "TransactionDetails"
-            WHERE
-              flight_id = f.id
-          ) "stock"
-			  FROM 
-			  	"Flights" f
-			  	INNER JOIN "Airports" dpA ON (f.departure_airport_id = dpA.id)
-			  	INNER JOIN "Airports" arA ON (f.arrival_airport_id = arA.id)
-			  	INNER JOIN "Airplanes" a ON (f.airplane_id = a.id)
-			  	INNER JOIN "Classes" c ON (a.class_id = c.id)
-			  	INNER JOIN "Cities" dpC ON (dpA.city_id = dpC.id)
-			  	INNER JOIN "Cities" arC ON (arA.city_id = arC.id)
-        WHERE
-          dpA.id = ${req.query.departure_airport_id} 
-          AND arA.id = ${req.query.destination_airport_id}
-          AND c.id = ${req.query.class_id}
-          AND f.stock >= ${req.query.number_passenger}
-          AND f.departure_date::varchar(255) LIKE '${req.query.departure_date}%'
-        ORDER BY 
-          f.departure_date
+      let list = [];
+      req.query.is_promo = (/true/).test(req.query.is_promo)
+
+      if(req.query.is_promo){
+        list = await sequelize.query(
+          `
+			    SELECT 
+            f.id,
+            dpA.id        "departure_airport_id",
+			    	dpA.name      "departure_airport",
+			    	dpA.code      "departure_airport_code",
+			    	dpC.name      "departure_city",
+			    	dpC.code      "departure_city_code",
+			    	dpC.time_zone "departure_city_time_zone",
+			    	f.departure_date,
+            arA.id        "arrival_airport_id",
+			    	arA.name      "arrival_airport",
+			    	arA.code      "arrival_airport_code",
+			    	arC.name      "arrival_city",
+			    	arC.code      "arrival_city_code",
+			    	arC.time_zone "arrival_city_time_zone",
+            arC.image     "arrival_city_image",
+			    	f.arrival_date,
+			    	a.name        "airplane_name",
+			    	a.logo        "airplane_logo",
+			    	a.code        "airplane_code",
+            c.id          "airplane_class_id",
+			    	c.name        "airplane_class",
+			    	f.price,
+			    	f.discount,
+			    	f.tax,
+			    	f.stock - (
+              SELECT 
+                COUNT(id)
+              FROM
+                "TransactionDetails"
+              WHERE
+                flight_id = f.id
+            ) "stock"
+          FROM 
+            "Flights" f
+            INNER JOIN "Airports" dpA ON (f.departure_airport_id = dpA.id)
+            INNER JOIN "Airports" arA ON (f.arrival_airport_id = arA.id)
+            INNER JOIN "Airplanes" a ON (f.airplane_id = a.id)
+            INNER JOIN "Classes" c ON (a.class_id = c.id)
+            INNER JOIN "Cities" dpC ON (dpA.city_id = dpC.id)
+            INNER JOIN "Cities" arC ON (arA.city_id = arC.id)
+          WHERE 
+            dpA.id = 1
+            AND arA.id = ${req.query.destination_airport_id}
+            AND f.id % 24 < 3
+            AND f.stock - (
+              SELECT 
+                COUNT(id)
+              FROM
+                "TransactionDetails"
+              WHERE
+                flight_id = f.id
+            ) >= ${req.query.number_passenger}
+            AND f.discount > 0
+            AND f.departure_date::date = '${req.query.departure_date}%'
+          ORDER 
+            BY f.departure_date
 			  `,
-        {
-          type: sequelize.QueryTypes.SELECT,
-        },
-      )
+          {
+            type: sequelize.QueryTypes.SELECT,
+          },
+        )
+      }else {
+        list = await sequelize.query(
+          `
+			    SELECT 
+            f.id,
+			    	dpA.name "departure_airport",
+			    	dpA.code "departure_airport_code",
+			    	dpC.name "departure_city",
+			    	dpC.code "departure_city_code",
+			    	dpC.time_zone "departure_city_time_zone",
+			    	f.departure_date,
+			    	arA.name "arrival_airport",
+			    	arA.code "arrival_airport_code",
+			    	arC.name "arrival_city",
+			    	arC.code "arrival_city_code",
+			    	arC.time_zone "arrival_city_time_zone",
+			    	f.arrival_date,
+			    	a.name "airplane_name",
+			    	a.logo "airplane_logo",
+			    	a.code "airplane_code",
+			    	c.name "airplane_class",
+			    	f.price,
+			    	f.discount,
+			    	f.tax,
+			    	f.stock - (
+              SELECT 
+                COUNT(id)
+              FROM
+                "TransactionDetails"
+              WHERE
+                flight_id = f.id
+            ) "stock"
+			    FROM 
+			    	"Flights" f
+			    	INNER JOIN "Airports" dpA ON (f.departure_airport_id = dpA.id)
+			    	INNER JOIN "Airports" arA ON (f.arrival_airport_id = arA.id)
+			    	INNER JOIN "Airplanes" a ON (f.airplane_id = a.id)
+			    	INNER JOIN "Classes" c ON (a.class_id = c.id)
+			    	INNER JOIN "Cities" dpC ON (dpA.city_id = dpC.id)
+			    	INNER JOIN "Cities" arC ON (arA.city_id = arC.id)
+          WHERE
+            dpA.id = ${req.query.departure_airport_id} 
+            AND arA.id = ${req.query.destination_airport_id}
+            AND c.id = ${req.query.class_id}
+            AND f.stock - (
+              SELECT 
+                COUNT(id)
+              FROM
+                "TransactionDetails"
+              WHERE
+                flight_id = f.id
+            ) >= ${req.query.number_passenger}
+            AND f.departure_date::varchar(255) LIKE '${req.query.departure_date}%'
+          ORDER BY 
+            f.departure_date
+			    `,
+          {
+            type: sequelize.QueryTypes.SELECT,
+          },
+        )
+      }
+
+      
 
       return res.status(200).json({
         success: true,

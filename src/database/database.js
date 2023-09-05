b71a370da00297831b2912875eb7fe41ea64acfa
AufaAsmawy19551 <aufamuhammad19551@gmail.com>
2023-09-06 02:47:04 +0700

    update

--- src/database/database.js
+++ src/database/database.js
@@ -1,33 +1,48 @@
 require('dotenv').config();
-const pg = require('pg')
+const pg = require('pg');
 const { DB_HOST, DB_PORT, DB_NAME, DB_USERNAME, DB_PASSWORD, DB_DIALECT = 'postgres' } = process.env;
 
 module.exports = {
-	development: {
-		host: DB_HOST,
-		database: DB_NAME,
-		username: DB_USERNAME,
-		password: DB_PASSWORD,
-		port: DB_PORT,
-		dialect: DB_DIALECT,
-		dialectModule: pg,
-	},
-	test: {
-		host: DB_HOST,
-		database: DB_NAME,
-		username: DB_USERNAME,
-		password: DB_PASSWORD,
-		port: DB_PORT,
-		dialect: DB_DIALECT,
-		dialectModule: pg,
-	},
-	production: {
-		host: DB_HOST,
-		database: DB_NAME,
-		username: DB_USERNAME,
-		password: DB_PASSWORD,
-		port: DB_PORT,
-		dialect: DB_DIALECT,
-		dialectModule: pg,
-	},
+  development: {
+    host: DB_HOST,
+    database: DB_NAME,
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    port: DB_PORT,
+    dialect: DB_DIALECT,
+    dialectModule: pg,
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false, // Set to true in production
+      },
+    },
+  },
+  test: {
+    host: DB_HOST,
+    database: DB_NAME,
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    port: DB_PORT,
+    dialect: DB_DIALECT,
+    dialectModule: pg,
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false, // Set to true in production
+      },
+    },
+  },
+  production: {
+    host: DB_HOST,
+    database: DB_NAME,
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    port: DB_PORT,
+    dialect: DB_DIALECT,
+    dialectModule: pg,
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: true, // Set to true in production
+      },
+    },
+  },
 };
